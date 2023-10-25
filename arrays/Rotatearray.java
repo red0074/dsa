6c270c4e28da2f1ad2dd7d6dd0181eeeda345e4f
red0074 <pdinesh122003@gmail.com>
2023-10-25 23:42:43 +0530

    array question

---
+++ arrays/Rotatearray.java
@@ -0,0 +1,9 @@
+// https://www.codingninjas.com/studio/problems/rotate-array_1230543?utm_source=youtube&utm_medium=affiliate&utm_campaign=parikh_youtube&leftPanelTab=1
+package dsa.arrays;
+
+import java.util.*;
+
+public class Rotatearray {
+    public static void main(String args[]) {
+    }
+}
